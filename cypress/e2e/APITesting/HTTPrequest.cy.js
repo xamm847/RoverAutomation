855d6be34245f12d4120098d5e9594bbb3aa4a20
Awais Shaukat <qa-mis@fam-llc.com>
2025-03-27 09:57:58 -0500

    updated code

---
+++ cypress/e2e/APITesting/HTTPrequest.cy.js
@@ -0,0 +1,54 @@
+
+describe("HTTPrequest",()=>{
+
+    it("Get call",()=>{
+
+            cy.request('GET','http://qa.rovermd.com:7777/mobile-users/searchUser/awais').its('status').should('eq',200)
+        
+    })
+
+    it("Post Call", ()=>{
+
+            cy.request({
+
+                method: 'POST',
+                url: 'http://qa.rovermd.com:7777/mobile-users/add',
+                body:{
+                    id: 1,
+                    clientIndex: 0,
+                    email: "test@test.com",
+                    firstName: "xamm",
+                    lastName: "khan",
+                    password: "abc@1234",
+                    userId: "xamm00011",
+                    userName: "test"
+                }
+            }).its('status').should('eq',201);
+
+            })
+
+    it.only('Put Call',()=>{
+
+                cy.request({
+
+                    method: 'PUT',
+                    url: 'http://qa.rovermd.com:7777/mobile-users/updateLockedStatus/1',     
+
+                })
+                .its('status').should('eq',200)
+
+
+
+            })
+
+
+
+
+
+    })
+    
+
+
+
+
+
